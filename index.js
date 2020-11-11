5beb0d78e6923144116bb7883d4132cd554141c0
irisabeldano <irislurdes45@gmail.com>
2020-11-11 18:30:33 -0300

    se añadio botones y bootstrap

--- index.js
+++ index.js
@@ -1,19 +1,6 @@
 
 alert("Bienvenida/o a mi pagina web!!")
 
-<script type="text/javascript">
-$(document).ready(function()
-{
-    $("#botella").mlens(
-    {
-        imgSrc: $("#botella").attr("data-big"),   // path of the hi-res version of the image
-        lensShape: "circle",                // shape of the lens (circle/square)
-        lensSize: 180,                  // size of the lens (in px)
-        borderSize: 4,                  // size of the lens border (in px)
-        borderColor: "#fff",                // color of the lens border (#hex)
-        borderRadius: 0,                // border radius (optional, only if the shape is square)
-        imgOverlay: $("#botella").attr("data-overlay"), // path of the overlay image (optional)
-        overlayAdapt: true // true if the overlay image has to adapt to the lens size (true/false)
-    });
-});
-</script>
+
+
+
